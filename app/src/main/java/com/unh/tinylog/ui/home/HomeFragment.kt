62d1abd60214fb2e20dfb6b4a7f625946ed30d60
Vijay <ssant16@unh.newhaven.edu>
2024-10-14 18:34:15 -0400

    Added navigation for home screen floating button

--- app/src/main/java/com/unh/tinylog/ui/home/HomeFragment.kt
+++ app/src/main/java/com/unh/tinylog/ui/home/HomeFragment.kt
@@ -1,12 +1,14 @@
 package com.unh.tinylog.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.unh.tinylog.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -33,6 +35,16 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAddChild.setOnClickListener{
+//            Log.d("MYTEST","Button Pressed")
+            val action = HomeFragmentDirections.actionNavigationHomeToAddEventFragment()
+            findNavController().navigate(action)
+        }
     }
 
     override fun onDestroyView() {
