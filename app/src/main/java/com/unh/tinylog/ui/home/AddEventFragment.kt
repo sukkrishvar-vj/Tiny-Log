0d91a02811d6cb29d2c5078bc662775c9f7f090e
Vijay <ssant16@unh.newhaven.edu>
2024-10-17 00:58:45 -0400

    Added functionality for seek age to change child age

--- app/src/main/java/com/unh/tinylog/ui/home/AddEventFragment.kt
+++ app/src/main/java/com/unh/tinylog/ui/home/AddEventFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
 import com.unh.tinylog.R
+import com.unh.tinylog.databinding.FragmentAddEventBinding
 
 class AddEventFragment : Fragment() {
 
@@ -17,6 +19,11 @@ class AddEventFragment : Fragment() {
     }
 
     private val viewModel: AddEventViewModel by viewModels()
+
+    private var _binding: FragmentAddEventBinding? = null
+    private val binding get() = _binding!!
+
+    private var childAgeDesc: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +37,31 @@ class AddEventFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_add_event, container, false)
+        _binding = FragmentAddEventBinding.inflate(inflater, container, false)
+
+        binding.switchGender.setOnClickListener{
+
+        }
+
+        binding.seekAge.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, currentVal: Int, p2: Boolean) {
+                when(currentVal){
+                    in 0..4 -> childAgeDesc = "Infant"
+                    in 5..12 -> childAgeDesc = "Toddler"
+                    in 13..24 -> childAgeDesc = "Baby"
+                }
+                binding.txtChildAge.text = "$currentVal Month Old $childAgeDesc"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_add_event, container, false)
     }
 }
